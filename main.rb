2e987a1cb7dc01fd7cab4c7bf53c20a5d49b95d4
Uduak Essien <acushla4real@gmail.com>
2020-06-16 09:30:52 +0100

    my_select method added

--- main.rb
+++ main.rb
@@ -20,20 +20,31 @@ module Enumerable
       counter += 1
     end
   end
+
+  def my_select
+    return to_enum unless block_given?
+    new_arr = []
+    my_each { |item| new_arr << item if yield(item) }
+    new_arr
+  end
+
 end
 
-[2, 5, 6, 7].my_each do |n|
-  puts n
-end
+# [2, 5, 6, 7].my_each do |n|
+#   puts n
+# end
 
-(1..5).my_each do |n|
-  puts n
-end
+# (1..5).my_each do |n|
+#   puts n
+# end
 
-[2, 5, 6, 7].my_each_with_index do |n, i|
-  puts i.to_s + ': ' + n.to_s
-end
+# [2, 5, 6, 7].my_each_with_index do |n, i|
+#   puts i.to_s + ': ' + n.to_s
+# end
 
-(1..5).my_each_with_index do |n, i|
-  puts i.to_s + ': ' + n.to_s
-end
+# (1..5).my_each_with_index do |n, i|
+#   puts i.to_s + ': ' + n.to_s
+# end
+
+
+p [2, 5, 6, 7].my_select { |n| n > 5 }
