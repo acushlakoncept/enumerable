2f81acd15835a4260be15c2a123819e4e713b834
Uduak Essien <acushla4real@gmail.com>
2020-06-16 09:08:00 +0100

    add my_each method

--- main.rb
+++ main.rb
@@ -1,1 +1,22 @@
+module Enumerable
 
+  def my_each
+    return to_enum unless block_given?
+    arr = self.class == Array ? self : to_a
+    counter = 0
+    while counter < arr.length
+      yield arr[counter]
+      counter += 1
+    end
+  end
+
+end
+
+
+[2,5,6,7].my_each do |n|
+  puts n
+end
+
+(1...5).my_each do |n|
+  puts n
+end
