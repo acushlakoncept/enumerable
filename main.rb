278d713343a4d534fbc034d7e03d58615c9b7e7d
jacobrees <jacob.rees97@yahoo.com>
2020-06-16 09:21:03 +0100

    Add my each with index method, fix linter errors with rubocop

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 module Enumerable
-
   def my_each
     return to_enum unless block_given?
+
     arr = self.class == Array ? self : to_a
     counter = 0
     while counter < arr.length
@@ -10,13 +10,30 @@ module Enumerable
     end
   end
 
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    arr = self.class == Array ? self : to_a
+    counter = 0
+    while counter < arr.length
+      yield(arr[counter], counter)
+      counter += 1
+    end
+  end
 end
 
-
-[2,5,6,7].my_each do |n|
+[2, 5, 6, 7].my_each do |n|
   puts n
 end
 
-(1...5).my_each do |n|
+(1..5).my_each do |n|
   puts n
+end
+
+[2, 5, 6, 7].my_each_with_index do |n, i|
+  puts i.to_s + ': ' + n.to_s
+end
+
+(1..5).my_each_with_index do |n, i|
+  puts i.to_s + ': ' + n.to_s
 end
