2fed643cf99b78bb2b4f3c3414e50c029240427d
jacobrees <jacob.rees97@yahoo.com>
2020-06-18 11:16:15 +0100

    Add main.sh file for repl, remove puts so functions return correct answer rather than printing it

--- main.rb
+++ main.rb
@@ -56,14 +56,9 @@ module Enumerable
     arr = self.class == Array ? self : to_a
     return arr.length unless block_given? || num
 
-    count = 0
-    if num
-      my_each { |item| count += 1 if item == num }
-    else
-      my_each { |item| count += 1 if yield(item) }
-    end
+    return arr.my_select { |item| item == num }.length if num
 
-    count
+    arr.my_select { |item| yield(item) }.length
   end
 
   def my_map(proc = nil)
@@ -81,20 +76,19 @@ module Enumerable
   def my_inject(num = 0)
     return to_enum unless block_given? || num
 
-    accumulator = 0
-
     if num
       accumulator = num
       my_each do |item|
         accumulator = yield(accumulator, item)
       end
-    else
-      my_each do |item|
-        accumulator = yield(accumulator, item)
-      end
+      return accumulator
     end
 
-    accumulator
+    sum = 0
+    my_each do |item|
+      sum = yield(sum, item)
+    end
+    sum
   end
 end
 
@@ -126,41 +120,41 @@ puts 'my_select'
 result = [2, 5, 6, 7].my_select do |n|
   n > 5
 end
-p result
+
 
 puts 'my_all'
 result = [2, 4, 6, 7, 8, 4].my_all? do |n|
   n < 9
 end
-p result
+
 
 puts 'my_any'
-p([4, 5, 6].my_any? { |n| n < 3 })
+([4, 5, 6].my_any? { |n| n < 3 })
 
 puts 'my_none'
-p([4, 5, 6].my_none? { |n| n > 5 })
+([4, 5, 6].my_none? { |n| n > 5 })
 
 puts 'my_count Array'
-p [2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count(2)
+[2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count(3)
 
 puts 'my_count Range'
-p (0..5).my_count(2)
+(0..5).my_count(2)
 
 puts 'my_map Range'
-p(0..5).my_map { |i| i * i }
+((0..5).my_map { |i| i * i })
 
 puts 'my_map Array'
-p([2, 5, 7, 4, 2].my_map { |i| i + 8 })
+([2, 5, 7, 4, 2].my_map { |i| i + 8 })
 
 puts 'my_map proc'
 my_proc = proc { |i| i * i }
-p [2, 5, 7, 4, 2].my_map(&my_proc)
+[2, 5, 7, 4, 2].my_map(&my_proc)
 
 puts 'my_inject Range'
-p(5..10).my_inject(1) { |x, y| x + y }
+((5..10).my_inject { |x, y| x + y })
 
 puts 'my_inject Array'
-p [5, 5, 7, 8].my_inject(1) { |x, y| x * y }
+[5, 5, 7, 8].my_inject(1) { |x, y| x * y }
 
 puts 'multiply_els'
-p multiply_els([2, 4, 5])
+multiply_els([2, 4, 5])
---
+++ main.sh
@@ -0,0 +1,1 @@
+irb main.rb
