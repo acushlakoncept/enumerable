483da9a1c275c4d52828dd3cc039e2bdc90e2b12
Uduak Essien <acushla4real@gmail.com>
2020-06-19 10:51:49 +0100

    add nil or false check on my_all? method to test for falsy value on no block given

--- main.rb
+++ main.rb
@@ -31,8 +31,21 @@ module Enumerable
     new_arr
   end
 
-  def my_all?
-    return to_enum(:my_all?) unless block_given?
+  def my_all?(arg = nil)
+    # return true if !&block && my_each { |n| n ? "truthy" : "falsy" }
+
+    # return to_enum(:my_all?) unless block_given?
+    # is_truthy = var ? "truthy" : "falsy"
+
+#     When no block or argument is given my_all? should return true when all the elements in the enum and truthy values and return false if at least one element in the collection is a falsy value.
+# example [1, 2, 3].my_all? should return true and [nil, 1, 2, 3].my_all? should return false.
+
+    if !block_given? && arg == nil 
+      my_each { |n| return false if n == nil || n == false}
+      return true
+    end
+
+    # return true unless block_given? || arg
 
     my_each do |item|
       return false if yield(item) == false
@@ -98,63 +111,67 @@ def multiply_els(arr)
   arr.my_inject(1) { |result, element| result * element }
 end
 
-puts 'my_each Array'
-[2, 5, 6, 7].my_each do |n|
-  puts n
-end
+# puts 'my_each Array'
+# [2, 5, 6, 7].my_each do |n|
+#   puts n
+# end
 
-puts 'my_each Range'
-(1..5).my_each do |n|
-  puts n
-end
+# puts 'my_each Range'
+# (1..5).my_each do |n|
+#   puts n
+# end
 
-puts 'my_each_with_index Array'
-[2, 5, 6, 7].my_each_with_index do |n, i|
-  puts i.to_s + ': ' + n.to_s
-end
+# puts 'my_each_with_index Array'
+# [2, 5, 6, 7].my_each_with_index do |n, i|
+#   puts i.to_s + ': ' + n.to_s
+# end
 
-puts 'my_each_with_index Range'
-(1..5).my_each_with_index do |n, i|
-  puts i.to_s + ': ' + n.to_s
-end
+# puts 'my_each_with_index Range'
+# (1..5).my_each_with_index do |n, i|
+#   puts i.to_s + ': ' + n.to_s
+# end
 
-puts 'my_select'
-[2, 5, 6, 7].my_select do |n|
-  n > 5
-end
+# puts 'my_select'
+# [2, 5, 6, 7].my_select do |n|
+#   n > 5
+# end
 
-puts 'my_all'
-[2, 4, 6, 7, 8, 4].my_all? do |n|
-  n < 9
-end
+# puts 'my_all'
+# [2, 4, 6, 7, 8, 4].my_all? do |n|
+#   n < 9
+# end
 
-puts 'my_any'
-([4, 5, 6].my_any? { |n| n < 3 })
+# puts 'my_any'
+# ([4, 5, 6].my_any? { |n| n < 3 })
 
-puts 'my_none'
-([4, 5, 6].my_none? { |n| n > 5 })
+# puts 'my_none'
+# ([4, 5, 6].my_none? { |n| n > 5 })
 
-puts 'my_count Array'
-[2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count(3)
+# puts 'my_count Array'
+# [2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count(3)
 
-puts 'my_count Range'
-(0..5).my_count(2)
+# puts 'my_count Range'
+# (0..5).my_count(2)
 
-puts 'my_map Range'
-((0..5).my_map { |i| i * i })
+# puts 'my_map Range'
+# ((0..5).my_map { |i| i * i })
 
-puts 'my_map Array'
-([2, 5, 7, 4, 2].my_map { |i| i + 8 })
+# puts 'my_map Array'
+# ([2, 5, 7, 4, 2].my_map { |i| i + 8 })
 
-puts 'my_map proc'
-my_proc = proc { |i| i * i }
-[2, 5, 7, 4, 2].my_map(&my_proc)
+# puts 'my_map proc'
+# my_proc = proc { |i| i * i }
+# [2, 5, 7, 4, 2].my_map(&my_proc)
 
-puts 'my_inject Range'
-((5..10).my_inject { |x, y| x + y })
+# puts 'my_inject Range'
+# ((5..10).my_inject { |x, y| x + y })
 
-puts 'my_inject Array'
-[5, 5, 7, 8].my_inject(1) { |x, y| x * y }
+# puts 'my_inject Array'
+# [5, 5, 7, 8].my_inject(1) { |x, y| x * y }
 
-puts 'multiply_els'
-multiply_els([2, 4, 5])
+# puts 'multiply_els'
+# multiply_els([2, 4, 5])
+
+p [1, 2, 3, nil].my_all? 
+p [2, 4, 6, 7, 8, 4].my_all? { |n| n > 2 }
+
