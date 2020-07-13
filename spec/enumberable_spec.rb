f3048d9b6e28d2b73fba352a7c98cac99ad413ed
Uduak Essien <acushla4real@gmail.com>
2020-07-13 10:06:34 +0100

    add rspec test for my_each when an array is given

---
+++ spec/enumberable_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/enumerable.rb'
+
+describe "#my_each" do
+  let(:test_arr) {[3,5,6]}
+  let(:test_range) {(1..5)}
+  context "Where an Array and block is given" do
+    it "should loop through the array" do
+      expect(test_arr.my_each{|x| x }).to eq([3,5,6])
+    end
+  end
+end
